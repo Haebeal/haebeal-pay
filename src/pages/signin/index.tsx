a086888cc2207ec9ccafcc5374aec955ea8ed47c
rom6621 <3103rom@gmail.com>
2023-10-09 22:08:08 +0900

    fix: サインインページのサイト名を変更

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -22,7 +22,7 @@ export const SigninPage = () => {
       <Center minH="calc(100vh - 80px)">
         <Box gap={4} minW="75%" bg="white" rounded="md" px={14} py={10}>
           <VStack gap={3}>
-            <Heading size="lg">Hebel Site</Heading>
+            <Heading size="lg">Haebeal Pay</Heading>
             <Divider />
             <Button minW="80%" colorScheme="twitter" onClick={signin}>
               Googleでログイン
