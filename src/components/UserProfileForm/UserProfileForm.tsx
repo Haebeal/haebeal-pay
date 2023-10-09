05f302c48ef884bbca6278736fc9347dcc2ba7bb
rom6621 <3103rom@gmail.com>
2023-10-10 02:01:47 +0900

    feat: 振込先情報フォームを作成

--- src/components/UserProfileForm/UserProfileForm.tsx
+++ src/components/UserProfileForm/UserProfileForm.tsx
@@ -4,7 +4,9 @@ import {
   FormControl,
   FormLabel,
   Heading,
+  HStack,
   Input,
+  Select,
   Skeleton,
   Spacer,
   Stack,
@@ -39,6 +41,7 @@ export const UserProfileForm = () => {
     register,
     reset,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<User>();
   const onSubmit: SubmitHandler<User> = async (formData) => {
@@ -64,6 +67,46 @@ export const UserProfileForm = () => {
     });
     setLoading(false);
   }, []);
+
+  const banks: {
+    code: string;
+    name: string;
+  }[] = [
+    {
+      code: "0005",
+      name: "三菱UFJ銀行",
+    },
+  ];
+  const getBranch = async () => {
+    const bankCode = watch("bankCode");
+    if (bankCode === "") {
+      toast({
+        status: "error",
+        title: "金融機関を選択してください",
+      });
+      return;
+    }
+    const branchCode = watch("branchCode");
+    if (branchCode === "") {
+      toast({
+        status: "error",
+        title: "店番を入力してください",
+      });
+      return;
+    }
+    const API_TOKEN = "CNKsD8zZE1qLzPCrzyzDpGLzr2twcCS9eF3xFzM";
+    const response = await fetch(
+      `https://apis.bankcode-jp.com/v3/banks/${bankCode}/branches/${branchCode}`,
+      {
+        method: "POST",
+        headers: {
+          apiKey: API_TOKEN,
+        },
+      }
+    );
+    const result = await response.json();
+    console.log(result);
+  };
 
   return (
     <Skeleton isLoaded={!isLoading}>
@@ -183,7 +226,101 @@ export const UserProfileForm = () => {
             />
           </Stack>
         </FormControl>
-        <Button type="submit" w="80%" colorScheme="twitter">
+        <Divider />
+        <Heading w={{ base: "" }} pt={2} size="sm" noOfLines={1}>
+          振込先情報
+        </Heading>
+        <FormControl isInvalid={errors.photoURL ? true : false}>
+          <Stack direction={{ base: "column", md: "row" }}>
+            <Heading
+              w={{ base: "", md: "30%" }}
+              as={FormLabel}
+              pt={2}
+              size="sm"
+              noOfLines={1}
+            >
+              金融機関
+            </Heading>
+            <Select
+              placeholder="金融機関を選択"
+              {...register("bankCode")}
+              required
+              size="md"
+              bg="gray.200"
+              border="none"
+            >
+              {banks.map((bank) => (
+                <option key={bank.code} value={bank.code}>
+                  {bank.name}
+                </option>
+              ))}
+            </Select>
+          </Stack>
+        </FormControl>
+        <FormControl isInvalid={errors.branchCode ? true : false}>
+          <Stack direction={{ base: "column", md: "row" }}>
+            <Heading
+              w={{ base: "", md: "30%" }}
+              as={FormLabel}
+              pt={2}
+              size="sm"
+              noOfLines={1}
+            >
+              支店・出張所
+            </Heading>
+            <HStack w="full">
+              <Input
+                maxW={300}
+                placeholder="支店・出張所名"
+                value={""}
+                noOfLines={1}
+                size="md"
+                border="none"
+                type="text"
+                readOnly
+              />
+              <Spacer />
+              <Input
+                maxW={200}
+                placeholder="店番を入力"
+                {...register("branchCode")}
+                noOfLines={1}
+                size="md"
+                bg="gray.200"
+                border="none"
+                type="number"
+                required
+              />
+              <Button colorScheme="twitter" onClick={getBranch}>
+                検索
+              </Button>
+            </HStack>
+          </Stack>
+        </FormControl>
+        <FormControl isInvalid={errors.accountCode ? true : false}>
+          <Stack direction={{ base: "column", md: "row" }}>
+            <Heading
+              w={{ base: "", md: "30%" }}
+              as={FormLabel}
+              pt={2}
+              size="sm"
+              noOfLines={1}
+            >
+              口座番号
+            </Heading>
+            <Input
+              placeholder="口座番号を入力"
+              {...register("accountCode")}
+              noOfLines={1}
+              size="md"
+              bg="gray.200"
+              border="none"
+              type="number"
+              required
+            />
+          </Stack>
+        </FormControl>
+        <Button mt={5} type="submit" w="80%" colorScheme="twitter">
           更新
         </Button>
       </VStack>
--- src/types/User.ts
+++ src/types/User.ts
@@ -2,4 +2,7 @@ export interface User {
   id: string;
   name: string;
   photoURL: string;
+  bankCode: string;
+  branchCode: string;
+  accountCode: string;
 }
