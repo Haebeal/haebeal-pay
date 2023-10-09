87dcd5030ca9683748255d47e9d5d8f321f5e93c
rom6621 <3103rom@gmail.com>
2023-10-09 20:42:11 +0900

    feat: パスワード認証を追加

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -5,13 +5,23 @@ import {
   Container,
   Divider,
   Heading,
+  Input,
   VStack,
 } from "@chakra-ui/react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
+import { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export const SigninPage = () => {
   const { signin, currentUser } = useAuth();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const signinWithPassword = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password);
+  };
 
   if (currentUser) {
     return <Navigate to="/" />;
@@ -29,6 +39,34 @@ export const SigninPage = () => {
             <Divider />
             <Button minW="80%" colorScheme="twitter" onClick={signin}>
               Googleでログイン
+            </Button>
+          </VStack>
+          <VStack mt={3}>
+            <Divider />
+            <Input
+              placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              noOfLines={1}
+              size="md"
+              bg="gray.200"
+              border="none"
+              type="text"
+              required
+            />
+            <Input
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              noOfLines={1}
+              size="md"
+              bg="gray.200"
+              border="none"
+              type="text"
+              required
+            />
+            <Button colorScheme="twitter" onClick={signinWithPassword}>
+              SignIn with Password
             </Button>
           </VStack>
         </Box>
